705be955c64841d1bbcb96ea160d5d1fece0cc6d
Morgan Jordan <sheffieldjordan@gmail.com>
2018-02-13 10:39:03 -0800

    Add files via upload
    Lab1 Solidity homeworks

---
+++ Greeter.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.18;
+
+contract Greeter {
+
+    string greeting;
+
+    function Greeter2(string _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting() public view returns(string){
+        return greeting;
+    }
+}
